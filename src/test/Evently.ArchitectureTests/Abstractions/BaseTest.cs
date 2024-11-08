f51ac4b146ed7877ab1bebd9b0bca30cc7682c36
avalserg <avalserg@gmail.com>
2024-11-08 10:11:58 +0300

    Add architecture tests

---
+++ src/test/Evently.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Evently.ArchitectureTests.Abstractions;
+public abstract class BaseTest
+{
+    protected const string UsersNamespace = "Evently.Modules.Users";
+    protected const string UsersIntegrationEventsNamespace = "Evently.Modules.Users.IntegrationEvents";
+
+    protected const string EventsNamespace = "Evently.Modules.Events";
+    protected const string EventsIntegrationEventsNamespace = "Evently.Modules.Events.IntegrationEvents";
+
+    protected const string TicketingNamespace = "Evently.Modules.Ticketings";
+    protected const string TicketingIntegrationEventsNamespace = "Evently.Modules.Ticketings.IntegrationEvents";
+
+    protected const string AttendanceNamespace = "Evently.Modules.Attendance";
+    protected const string AttendanceIntegrationEventsNamespace = "Evently.Modules.Attendance.IntegrationEvents";
+}
